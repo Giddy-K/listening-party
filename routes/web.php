745d4f849ace676410e6bb29c28ebbc6ab985b40
Giddy-K <gideonkipamet@gmail.com>
2025-03-09 13:16:24 +0300

    Modified the web routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-Route::view('/', 'home');
+Route::view('/', 'home')->name('home');
+
+Volt::route('/parties/{listeningParty}', 'pages.parties.show')->name('parties.show');
 
 require __DIR__.'/auth.php';
