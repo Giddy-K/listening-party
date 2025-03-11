48643aa6dc1e8ff31ebcd1e29b1ec5123089e6f0
Giddy-K <gideonkipamet@gmail.com>
2025-03-11 19:30:14 +0300

    Modified the apps layout components

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,26 @@
-<x-layouts.app.sidebar>
-    <flux:main>
-        {{ $slot }}
-    </flux:main>
-</x-layouts.app.sidebar>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Awwdio - Let's Listen Together</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link
+        href="https://fonts.bunny.net/css?family=figtree:400,600|aleo:300,500,700|annie-use-your-telescope:400&display=swap"
+        rel="stylesheet" />
+
+    <wireui:scripts />
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="bg-emerald-50">
+    <x-button class="absolute z-50 top-4 left-4" xs flat href="/">← Home</x-button>
+    {{ $slot }}
+</body>
+
+</html>
