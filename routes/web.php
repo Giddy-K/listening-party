1f6ae68920c687a4c31e19fe5bfb3508ceb64e92
Giddy-K <gideonkipamet@gmail.com>
2026-06-05 02:02:00 +0300

    Create routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Livewire\Volt\Volt;
 
 Route::view('/', 'home')->name('home');
 
+Volt::route('/dashboard', 'dashboard')->name('dashboard');
+
 Volt::route('/parties/{listeningParty}', 'pages.parties.show')->name('parties.show');
 
 require __DIR__.'/auth.php';
